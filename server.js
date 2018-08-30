a7863ece9a47a569b354620159ee864efd4766e8
Stelyan Baltov <stelyan.baltov@gmail.com>
2018-08-30 14:10:25 +0300

    update server.js PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const webpackConfig = require(`./webpack/webpack.${NODE_ENV}.config.js`);
 const compiler = webpack(webpackConfig);
 
 const app = express();
-const port = 4300;
+const port = process.env.PORT || 4300;
 
 app.use(
     webpackDevMiddleware(compiler, {

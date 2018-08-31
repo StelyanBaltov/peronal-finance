846ea11ad650b880f50b3bd25e2fbb06ddaaf1f6
Stelyan Baltov <stelyan.baltov@gmail.com>
2018-08-31 16:41:17 +0300

    add nodemon

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ const path = require("path");
 const webpack = require("webpack");
 const webpackConfig = require(`./webpack/webpack.${NODE_ENV}.config.js`);
 const compiler = webpack(webpackConfig);
+const httpProxy = require('http-proxy');
 
 const app = express();
+
+const apiHost = 'localhost';
+const apiPort = 2003;
+
 const port = process.env.PORT || 4300;
+
+const Proxy = httpProxy.createProxyServer({
+    target: {
+        host: apiHost,
+        port: apiPort
+    }
+});
 
 app.use(
     webpackDevMiddleware(compiler, {
@@ -19,8 +31,14 @@ app.use(
 
 app.use(webpackHotMiddleware(compiler, {}));
 
+app.get(['/users'], (req, res) => {
+    req.headers.host = apiHost;
+    Proxy.web(req, res)
+});
+
 app.get("*", function (req, res) {
     res.sendFile(path.resolve(path.join(__dirname, "dist", "index.html")));
 });
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
